50382155681843c45d57b63daf1b3b0b7d559438
Josh Humphries <2035234+jhump@users.noreply.github.com>
2023-06-09 23:33:07 -0400

    clarify comments on picker.Factory about mutating in-place being okay

--- balancer/picker/picker.go
+++ balancer/picker/picker.go
@@ -40,16 +40,21 @@ type Picker interface {
 // Factory creates new Picker instances.
 type Factory interface {
 	// New creates a new picker that will select a connection from the given
-	// set. The previous picker is provided so that successive "generations"
-	// of pickers can share state. This is necessary for something like a
-	// least-loaded algorithm, where tracking the number of active operations
-	// per connection needs to persist from one picker to the next. Note that
-	// the previous picker may still be in use, concurrently, while the factory
-	// is creating the new one, and even for some small amount of time after this
-	// method returns. Also, operations might be started with the previous picker
-	// but not completed until after the new picker is put in use. Picker
-	// factory implementations need to use care for thread-safety when pickers
-	// need to share state.
+	// set.
+	//
+	// The previous picker is provided so that successive "generations" of
+	// pickers can share state. In fact, it is entirely acceptable for this
+	// function to *mutate* the previous picker and return it, instead of
+	// returning a separate instance. This flexibility is useful for stateful
+	// algorithms like least-loaded, where tracking the number of active
+	// operations per connection needs to persist from one generation to the
+	// next. Note that the previous picker may still be in use, concurrently,
+	// while the factory is creating the new one (or modifying the previous
+	// one), and even for some small amount of time after this method returns.
+	// Also, operations might be started with the previous picker but not
+	// completed until after the new picker is put in use. Picker factory
+	// implementations need to use care for thread-safety when pickers need
+	// to share state.
 	//
 	// This method will never be called with an empty set of connections. There
 	// will always be at least one connection.
