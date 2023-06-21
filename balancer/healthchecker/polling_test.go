7580b7b9d5916bac58923cc4ec91f8f0bb623f4d
John Chadwick <jchadwick@buf.build>
2023-06-21 16:08:05 -0400

    Update polling tests to test interval, threshold

--- balancer/healthchecker/polling_test.go
+++ balancer/healthchecker/polling_test.go
@@ -12,82 +12,117 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package healthchecker_test
+package healthchecker
 
 import (
 	"context"
+	"errors"
 	"net/http"
-	"sync"
 	"testing"
+	"time"
 
-	"github.com/bufbuild/go-http-balancer/balancer/balancertesting"
+	"github.com/bufbuild/go-http-balancer/attrs"
 	"github.com/bufbuild/go-http-balancer/balancer/conn"
-	"github.com/bufbuild/go-http-balancer/balancer/healthchecker"
+	"github.com/bufbuild/go-http-balancer/internal/clocktest"
 	"github.com/bufbuild/go-http-balancer/resolver"
 	"github.com/stretchr/testify/assert"
 )
 
-type fakeHealthTracker func(conn conn.Conn, health healthchecker.HealthState)
+type fakeConn chan *http.Response
 
-func (f fakeHealthTracker) UpdateHealthState(conn conn.Conn, health healthchecker.HealthState) {
-	f(conn, health)
+func (f fakeConn) RoundTrip(*http.Request, func()) (*http.Response, error) {
+	response := <-f
+	if response == nil {
+		return nil, errors.New("fake error")
+	}
+	return response, nil
 }
 
-type fakeConn struct {
-	conn.Conn
-	response *http.Response
+func (f fakeConn) Scheme() string {
+	return "http"
 }
 
-func (f *fakeConn) RoundTrip(*http.Request, func()) (*http.Response, error) {
-	return f.response, nil
+func (f fakeConn) Address() resolver.Address {
+	return resolver.Address{HostPort: "::1"}
 }
 
-func expectHealth(t *testing.T, expected healthchecker.HealthState, done func()) healthchecker.HealthTracker { //nolint:thelper
-	return fakeHealthTracker(func(conn conn.Conn, health healthchecker.HealthState) {
-		assert.Equal(t, expected, health)
-		done()
-	})
+func (f fakeConn) UpdateAttributes(attrs.Attributes) {}
+
+type fakeHealthTracker chan HealthState
+
+func (f fakeHealthTracker) UpdateHealthState(_ conn.Conn, health HealthState) {
+	f <- health
 }
 
 func TestPollingChecker(t *testing.T) {
 	t.Parallel()
 
-	pool := balancertesting.NewFakeConnPool()
-	newConn, _ := pool.NewConn(resolver.Address{})
-	checker := healthchecker.NewPollingChecker(healthchecker.PollingCheckerConfig{}, healthchecker.NewSimpleProber("/"))
+	testClock := clocktest.NewFakeClock()
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	t.Cleanup(cancel)
+	checker := NewPollingChecker(PollingCheckerConfig{}, NewSimpleProber("/"))
+	checker.(*pollingChecker).clock = testClock
+	tracker := make(fakeHealthTracker, 1)
 
 	// Unhealthy (HTTP error)
-	waitGroup := new(sync.WaitGroup)
-	waitGroup.Add(1)
-	process := checker.New(
-		context.Background(),
-		newConn,
-		expectHealth(t, healthchecker.Unhealthy, waitGroup.Done),
-	)
-	waitGroup.Wait()
-	process.Close()
+	conn := make(fakeConn)
+	close(conn)
+	checker.New(ctx, conn, tracker).Close()
+	assert.Equal(t, Unhealthy, <-tracker)
 
-	// Unhealthy (HTTP 500)
-	waitGroup = new(sync.WaitGroup)
-	waitGroup.Add(1)
-	response := &http.Response{StatusCode: http.StatusInternalServerError, Body: http.NoBody}
-	process = checker.New(
-		context.Background(),
-		&fakeConn{Conn: newConn, response: response},
-		expectHealth(t, healthchecker.Unhealthy, waitGroup.Done),
-	)
-	waitGroup.Wait()
-	process.Close()
+	// Unhealthy (HTTP 5xx)
+	conn = make(fakeConn, 1)
+	conn <- &http.Response{StatusCode: http.StatusBadGateway, Body: http.NoBody}
+	checker.New(ctx, conn, tracker).Close()
+	assert.Equal(t, Unhealthy, <-tracker)
+	close(conn)
 
-	// Healthy (HTTP 200)
-	waitGroup = new(sync.WaitGroup)
-	waitGroup.Add(1)
-	response = &http.Response{StatusCode: http.StatusOK, Body: http.NoBody}
-	process = checker.New(
-		context.Background(),
-		&fakeConn{Conn: newConn, response: response},
-		expectHealth(t, healthchecker.Healthy, waitGroup.Done),
-	)
-	waitGroup.Wait()
+	// Healthy (HTTP 2xx)
+	conn = make(fakeConn, 1)
+	conn <- &http.Response{StatusCode: http.StatusOK, Body: http.NoBody}
+	checker.New(ctx, conn, tracker).Close()
+	assert.Equal(t, Healthy, <-tracker)
+	close(conn)
+}
+
+func TestPollingCheckerThresholds(t *testing.T) {
+	t.Parallel()
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	t.Cleanup(cancel)
+
+	interval := 5 * time.Second
+	testClock := clocktest.NewFakeClock()
+
+	checker := NewPollingChecker(PollingCheckerConfig{
+		PollingInterval:    interval,
+		HealthyThreshold:   2,
+		UnhealthyThreshold: 3,
+	}, NewSimpleProber("/"))
+	checker.(*pollingChecker).clock = testClock
+
+	conn := make(fakeConn)
+	tracker := make(fakeHealthTracker)
+	process := checker.New(ctx, conn, tracker)
+	advance := func(response *http.Response) {
+		t.Helper()
+		conn <- response
+		err := testClock.BlockUntilContext(ctx, 1)
+		assert.NoError(t, err)
+		testClock.Advance(interval)
+	}
+
+	// Require two healthy checks to pass
+	advance(&http.Response{StatusCode: http.StatusOK, Body: http.NoBody})
+	advance(&http.Response{StatusCode: http.StatusOK, Body: http.NoBody})
+	assert.Equal(t, Healthy, <-tracker)
+
+	// Require three unheatlhy checks to fail
+	advance(&http.Response{StatusCode: http.StatusBadGateway, Body: http.NoBody})
+	advance(&http.Response{StatusCode: http.StatusBadGateway, Body: http.NoBody})
+	advance(&http.Response{StatusCode: http.StatusBadGateway, Body: http.NoBody})
+	close(conn)
+	assert.Equal(t, Unhealthy, <-tracker)
+
 	process.Close()
 }
