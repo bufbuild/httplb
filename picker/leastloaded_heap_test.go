47d096845c37b27e03a3f732181f4b2fa3612ae6
Joshua Humphries <2035234+jhump@users.noreply.github.com>
2024-04-19 09:46:05 -0400

    Fix heap update operation for least-loaded picker (#61)
    The `update` method of the heap, which reconciles the set of connections
    with a new set of connections provided from a resolver, was sometimes
    calling `heap.Remove`, which not only would change the order of elements
    making the iteration invalid, but also shorten the slice, making the iteration
    eventually panic with an index-out-of-bounds failure.
    
    This changes the logic to not use `heap.Remove` during the iteration.
    Instead, it mutates the slice, which potentially breaks heap invariants. So
    instead of `heap.Push` for new items, it simply appends them, and then it
    does `heap.Init` to re-heapify the slice at the end.
    
    This also adds a new test to verify that the heap is working under a variety
    of scenarios, including updates that may need to add and remove connections.

---
+++ picker/leastloaded_heap_test.go
@@ -0,0 +1,201 @@
+// Copyright 2023 Buf Technologies, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package picker
+
+import (
+	"testing"
+
+	"github.com/bufbuild/httplb/conn"
+	"github.com/bufbuild/httplb/internal/conns"
+	"github.com/stretchr/testify/require"
+)
+
+func TestLeastLoadedConnHeap(t *testing.T) {
+	t.Parallel()
+	heap := newConnHeap(conns.FromSlice([]conn.Conn{
+		dummyConn{id: "a"},
+		dummyConn{id: "b"},
+		dummyConn{id: "c"},
+		dummyConn{id: "d"},
+		dummyConn{id: "e"},
+		dummyConn{id: "f"},
+	}))
+	counts := map[string]uint64{
+		"a": 0,
+		"b": 0,
+		"c": 0,
+		"d": 0,
+		"e": 0,
+		"f": 0,
+	}
+	verifyHeap(t, heap, counts)
+
+	// Note: order may not be intuitive, due to how
+	// nodes in the heap are sifted up and down as an item
+	// is popped, but it is deterministic.
+
+	// No repeats since they all have weight zero.
+	verifyPicks(t, heap, counts, "abdecf")
+	// Now they all have weight one, so they all repeat. But
+	// we don't see any item a third time until we've seen
+	// all of 'em 2x.
+	verifyPicks(t, heap, counts, "fdabce")
+
+	verifyReleases(t, heap, counts, "aabb")
+
+	// Now a and b have a load of zero, but the others have load 2.
+	// So we'll pick them next.
+	verifyPicks(t, heap, counts, "abba")
+
+	snapshot := snapshotHeap(heap)
+	// Update state with new connections. We should forget
+	// a, b, c, and d and add g and h.
+	heap.update(conns.FromSlice([]conn.Conn{
+		dummyConn{id: "e"},
+		dummyConn{id: "f"},
+		dummyConn{id: "g"},
+		dummyConn{id: "h"},
+	}))
+	counts = map[string]uint64{
+		"e": 2,
+		"f": 2,
+		"g": 0,
+		"h": 0,
+	}
+	verifyHeap(t, heap, counts)
+
+	// Releasing items no longer present has no impact.
+	heap.release(snapshot["a"])
+	heap.release(snapshot["b"])
+	heap.release(snapshot["c"])
+	heap.release(snapshot["a"])
+	verifyHeap(t, heap, counts)
+
+	// g and h have less load, so we favor them.
+	verifyPicks(t, heap, counts, "hggh")
+	// Now everything has load == 2. So next four picks sees
+	// each of the four items.
+	verifyPicks(t, heap, counts, "hefg")
+
+	// No-op update
+	heap.update(conns.FromSlice([]conn.Conn{
+		dummyConn{id: "h"},
+		dummyConn{id: "g"},
+		dummyConn{id: "f"},
+		dummyConn{id: "e"},
+	}))
+	verifyHeap(t, heap, counts)
+
+	// Update that must grow backing slice
+	heap.update(conns.FromSlice([]conn.Conn{
+		dummyConn{id: "a"},
+		dummyConn{id: "b"},
+		dummyConn{id: "c"},
+		dummyConn{id: "d"},
+		dummyConn{id: "e"},
+		dummyConn{id: "f"},
+		dummyConn{id: "g"},
+		dummyConn{id: "h"},
+		dummyConn{id: "i"},
+		dummyConn{id: "j"},
+		dummyConn{id: "k"},
+		dummyConn{id: "l"},
+	}))
+	counts = map[string]uint64{
+		"a": 0,
+		"b": 0,
+		"c": 0,
+		"d": 0,
+		"e": 3,
+		"f": 3,
+		"g": 3,
+		"h": 3,
+		"i": 0,
+		"j": 0,
+		"k": 0,
+		"l": 0,
+	}
+	verifyHeap(t, heap, counts)
+}
+
+func verifyPicks(t *testing.T, heap *leastLoadedConnHeap, counts map[string]uint64, ids string) {
+	t.Helper()
+	for _, ch := range ids {
+		id := string(ch)
+		item := heap.acquire(0)
+		require.Equal(t, id, connID(item.conn))
+		counts[id]++
+		verifyHeap(t, heap, counts)
+	}
+}
+
+func verifyReleases(t *testing.T, heap *leastLoadedConnHeap, counts map[string]uint64, ids string) {
+	t.Helper()
+	for _, ch := range ids {
+		id := string(ch)
+		release(t, heap, id)
+		counts[id]--
+		verifyHeap(t, heap, counts)
+	}
+}
+
+func release(t *testing.T, heap *leastLoadedConnHeap, id string) { //nolint:varnamelen
+	t.Helper()
+	for _, item := range *heap {
+		if connID(item.conn) == id {
+			heap.release(item)
+			return
+		}
+	}
+	t.Fatalf("item %s not found in heap", id)
+}
+
+func snapshotHeap(heap *leastLoadedConnHeap) map[string]*leastLoadedConnItem {
+	snapshot := make(map[string]*leastLoadedConnItem, len(*heap))
+	for _, item := range *heap {
+		snapshot[connID(item.conn)] = item
+	}
+	return snapshot
+}
+
+func verifyHeap(t *testing.T, heap *leastLoadedConnHeap, counts map[string]uint64) {
+	t.Helper()
+	for i, item := range *heap {
+		require.Equal(t, i, item.index)
+		count, ok := counts[connID(item.conn)]
+		require.True(t, ok)
+		require.Equal(t, count, item.load)
+		if i > 0 {
+			// heap invariant
+			parent := (i - 1) / 2
+			require.LessOrEqual(t, (*heap)[parent].load, item.load)
+		}
+	}
+	backingArray := (*heap)[:cap(*heap)]
+	for i := len(*heap); i < len(backingArray); i++ {
+		// make sure everything else in the backing array, after
+		// the end of the heap, is cleared and not pinning any item
+		require.Nil(t, backingArray[i])
+	}
+}
+
+type dummyConn struct {
+	conn.Conn
+	id string
+}
+
+func connID(cn conn.Conn) string {
+	return cn.(dummyConn).id
+}
